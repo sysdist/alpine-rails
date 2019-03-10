0214e7ed74a2d697d867fb94b9f9bfb023e8fec7
Stefan <sysdist.com@gmail.com>
2019-03-10 18:01:17 +0100

    docker-rails setup

--- run.sh
+++ run.sh
@@ -9,24 +9,22 @@ rm -f /data/tmp/pids/server.pid
 #     bundle exec rails new . --skip --skip-git --skip-bundle --skip-coffee --skip-spring 
 #     echo "recreated bin/* files"
 # fi
-bundle exec rails app:update:bin
+#bundle exec rails app:update:bin
 
 #export SECRET_KEY_BASE=`bundle exec rake secret`
 
 CONFIG_PATH=/data/options.json
 export SECRET_KEY_BASE="$(jq --raw-output '.secret_key_base' $CONFIG_PATH)"
-
+#export SECRET_KEY_BASE=fff
 echo "SECRET_KEY_BASE is $SECRET_KEY_BASE"
 
 export RAILS_ENV=production
 
 bundle exec rake db:create
 
-
-
-
-
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 #exec "$@"
 
-bundle exec rails server -b 0.0.0.0 -e production 
+#bundle exec rails server -b 0.0.0.0 -e production 
+
+bundle exec puma -C config/puma.rb
